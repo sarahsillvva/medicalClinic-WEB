4862e05cb7b8a8e53d973d7627997f25951f8415
Sarah Silva <sarah.silvaon@gmail.com>
2022-06-06 23:25:37 -0300

    aplication angular

---
+++ clinicalMedical-web/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatToolbarModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
